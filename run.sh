7984e54f90089e00c1f642f3e7a30ae4da6e849b
brook <min@sadais.com>
2023-11-16 00:37:16 +0800

    build: upd

--- run.sh
+++ run.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
-rm -rf /tmp/.X*-lock
-
-export DISPLAY=:0
-
-# Start dbus
-mkdir -p /var/run/dbus
-/usr/bin/dbus-daemon --session --fork
-
-# Start xvfb
-/usr/bin/Xvfb $DISPLAY -ac -screen 0 1920x1080x24 +extension RANDR &
+export DISPLAY=':99.0'
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 
 # Give time to dbus & Xvfb to boot
 sleep 1
